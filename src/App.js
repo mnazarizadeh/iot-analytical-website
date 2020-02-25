ef080bec6f75cca2d119e28ed7ac19c18916cd11
Ali Sadoughi <asadoughi1990@gmail.com>
2020-02-25 16:35:17 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "bootstrap";
+import "animate.css";
+import './assets/css/style.css';
+
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+     <React.Fragment>
+
+        <Navbar /> 
+
+     </React.Fragment>
+  );
+}
+
+export default App;
