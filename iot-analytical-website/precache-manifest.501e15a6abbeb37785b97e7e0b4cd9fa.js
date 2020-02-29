eff74ec39af7dd179359e8e6881416d64945f6fc
Mohammad <m.nazarizadeh@gmail.com>
2020-03-01 00:37:18 +0330

    Updates

---
+++ iot-analytical-website/precache-manifest.501e15a6abbeb37785b97e7e0b4cd9fa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa981b412f2ec97cdec394689a46ee54",
+    "url": "/iot-analytical-website/index.html"
+  },
+  {
+    "revision": "e4fe6ee13f36b349f973",
+    "url": "/iot-analytical-website/static/css/2.465a5db4.chunk.css"
+  },
+  {
+    "revision": "999e498f0b54a4440068",
+    "url": "/iot-analytical-website/static/css/main.e5ab829c.chunk.css"
+  },
+  {
+    "revision": "e4fe6ee13f36b349f973",
+    "url": "/iot-analytical-website/static/js/2.bf8f9035.chunk.js"
+  },
+  {
+    "revision": "bd7deba33a852702bce77feb55b1add7",
+    "url": "/iot-analytical-website/static/js/2.bf8f9035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999e498f0b54a4440068",
+    "url": "/iot-analytical-website/static/js/main.d16410d3.chunk.js"
+  },
+  {
+    "revision": "759f19c95ec1d4bea5dc",
+    "url": "/iot-analytical-website/static/js/runtime-main.d5dd44e7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/iot-analytical-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/iot-analytical-website/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/iot-analytical-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/iot-analytical-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/iot-analytical-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "20e8f8600db51020010f62b7cc172419",
+    "url": "/iot-analytical-website/static/media/intro-bg.20e8f860.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/iot-analytical-website/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/iot-analytical-website/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/iot-analytical-website/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/iot-analytical-website/static/media/ionicons.c037dbbc.svg"
+  }
+]);
