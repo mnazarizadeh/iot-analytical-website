5d39a20f7f1212bbc0a10f0d41b915507e864db8
Mohammad <m.nazarizadeh@gmail.com>
2020-02-29 23:26:56 +0330

    loginand dashboard added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,41 +1,149 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input
+} from "reactstrap";
+import SweetAlert from "sweetalert2-react";
+import { getValueFromEvent } from "../util/clientUtils";
 
 class Navbar extends Component {
-    
-    render() {
-        return (
-            <React.Fragment>
-
-                <header id="header" className="fixed-top">
-                    <div className="container">
-
-                        <div className="logo float-left">
-                            {/* Uncomment below if you prefer to use an image logo  */}
-                            <h2 className="text-light"><a href="#header"><span>IOT Analytical</span></a></h2>
-                            {/* <a href="#intro" className="scrollto"><img src="../assets/img/logo.png"
-                            alt="logo" className="img-fluid"/></a> */}
-                        </div>
-
-                        <nav className="main-nav float-right d-none d-lg-block">
-                            <ul>
-                                
-                                <li className="active"><a href="#intro">Home</a></li>
-                                <li><a href="#about">Features</a></li>
-                                <li><a href="#portfolio">Portfolio</a></li>
-                                <li><a href="#team">Team</a></li>
-                                <li><a href="#contact">Contact Us</a></li>
-                                <li><a href="/" id="btn-login">Log in</a></li>
-                               
-                            </ul>
-                            
-                        </nav> 
-                    </div>
-                </header>
-                
-            </React.Fragment>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      show: false,
+      iUsername: "",
+      Password: ""
+    };
+  }
+  openModal = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  };
+  CheckLogin = () => {
+    if (this.state.iUsername === "Ali" && this.state.Password === "sa123456") {
+      this.props.history.push(`/Dashboard`);
+    } else {
+      this.setState(prevState => ({
+        show: !prevState.show
+      }));
     }
+  };
+  onInputChangedUsername = e => {
+    const newValues = getValueFromEvent(e);
+    this.setState({
+      iUsername: newValues.Username
+    });
+  };
+  onInputChangedPassword = e => {
+    const newValues = getValueFromEvent(e);
+
+    this.setState({
+      Password: newValues.Password
+    });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <SweetAlert
+          icon="warning"
+          show={this.state.show}
+          title="Log in Failed"
+          text="Your username or password is incorrect, Please Try Again!"
+          onConfirm={() => this.setState({ show: false })}
+        />
+        <div>
+          <Modal isOpen={this.state.modal} toggle={() => this.openModal()}>
+            <ModalHeader toggle={() => this.openModal()}>Log in</ModalHeader>
+            <ModalBody>
+              <div>
+                <InputGroup>
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="fa fa-user"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    name="Username"
+                    placeholder="username"
+                    onChange={e => this.onInputChangedUsername(e)}
+                  />
+                </InputGroup>
+                <br />
+                <InputGroup>
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="fa fa-lock"></i>
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    name="Password"
+                    placeholder="Password"
+                    type="password"
+                    onChange={e => this.onInputChangedPassword(e)}
+                  />
+                </InputGroup>
+                <br />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={() => this.CheckLogin()}>
+                Log in
+              </Button>{" "}
+              <Button color="secondary" onClick={() => this.openModal()}>
+                Exit
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              <h3 id="logo-text">IOT ANALYTICAL</h3>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#intro">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#portfolio">Portfolio</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#team">Team</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link mr-3" href="#contact">Contact Us</a>
+                </li>
+              </ul>
+
+              <button class="btn btn-outline-primary bt-md my-2 my-sm-0 px-4" onClick={() => this.openModal()}>Log in</button>
+
+            </div>
+          </div>
+        </nav>
+
+      </React.Fragment>
+    );
+  }
 }
 
-export default Navbar;
+export default withRouter(Navbar);
+
--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Team extends Component {
-    
+
     render() {
         return (
             <React.Fragment>
@@ -9,84 +9,102 @@ class Team extends Component {
                 <section id="team">
                     <div class="container">
                         <div class="section-header">
-                        <h3>Team</h3>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem</p>
+                            <h3>Team</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem</p>
                         </div>
 
-                        <div class="row">
+                        <div class="row justify-content-center">
 
-                            <div class="col-lg-3 col-md-6 wow fadeInUp">
+                            <div class="col-lg-2 col-md-6 wow fadeInUp">
                                 <div class="member">
-                                <img src="assets/img/team-1.jpg" class="img-fluid" alt=""/>
-                                <div class="member-info">
-                                    <div class="member-info-content">
-                                    <h4>Walter White</h4>
-                                    <span>Chief Executive Officer</span>
-                                    <div class="social">
-                                        <a href="/"><i class="fa fa-twitter"></i></a>
-                                        <a href="/"><i class="fa fa-facebook"></i></a>
-                                        <a href="/"><i class="fa fa-google-plus"></i></a>
-                                        <a href="/"><i class="fa fa-linkedin"></i></a>
+                                    <img src="assets/img/team-1.jpg" class="img-fluid" alt="" />
+                                    <div class="member-info">
+                                        <div class="member-info-content">
+                                            <h4>Walter White</h4>
+                                            <span>Chief Executive Officer</span>
+                                            <div class="social">
+                                                <a href="/"><i class="fa fa-twitter"></i></a>
+                                                <a href="/"><i class="fa fa-facebook"></i></a>
+                                                <a href="/"><i class="fa fa-google-plus"></i></a>
+                                                <a href="/"><i class="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
                                     </div>
-                                    </div>
-                                </div>
                                 </div>
                             </div>
 
-                            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                                 <div class="member">
-                                <img src="assets/img/team-2.jpg" class="img-fluid" alt=""/>
-                                <div class="member-info">
-                                    <div class="member-info-content">
-                                    <h4>Sarah Jhonson</h4>
-                                    <span>Product Manager</span>
-                                    <div class="social">
-                                        <a href="/"><i class="fa fa-twitter"></i></a>
-                                        <a href="/"><i class="fa fa-facebook"></i></a>
-                                        <a href="/"><i class="fa fa-google-plus"></i></a>
-                                        <a href="/"><i class="fa fa-linkedin"></i></a>
+                                    <img src="assets/img/team-2.jpg" class="img-fluid" alt="" />
+                                    <div class="member-info">
+                                        <div class="member-info-content">
+                                            <h4>Sarah Jhonson</h4>
+                                            <span>Product Manager</span>
+                                            <div class="social">
+                                                <a href="/"><i class="fa fa-twitter"></i></a>
+                                                <a href="/"><i class="fa fa-facebook"></i></a>
+                                                <a href="/"><i class="fa fa-google-plus"></i></a>
+                                                <a href="/"><i class="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
                                     </div>
-                                    </div>
-                                </div>
                                 </div>
                             </div>
 
-                            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
                                 <div class="member">
-                                <img src="assets/img/team-3.jpg" class="img-fluid" alt=""/>
-                                <div class="member-info">
-                                    <div class="member-info-content">
-                                    <h4>William Anderson</h4>
-                                    <span>CTO</span>
-                                    <div class="social">
-                                        <a href="/"><i class="fa fa-twitter"></i></a>
-                                        <a href="/"><i class="fa fa-facebook"></i></a>
-                                        <a href="/"><i class="fa fa-google-plus"></i></a>
-                                        <a href="/"><i class="fa fa-linkedin"></i></a>
+                                    <img src="assets/img/team-3.jpg" class="img-fluid" alt="" />
+                                    <div class="member-info">
+                                        <div class="member-info-content">
+                                            <h4>William Anderson</h4>
+                                            <span>CTO</span>
+                                            <div class="social">
+                                                <a href="/"><i class="fa fa-twitter"></i></a>
+                                                <a href="/"><i class="fa fa-facebook"></i></a>
+                                                <a href="/"><i class="fa fa-google-plus"></i></a>
+                                                <a href="/"><i class="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
                                     </div>
-                                    </div>
-                                </div>
                                 </div>
                             </div>
 
-                            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                            <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                                 <div class="member">
-                                <img src="assets/img/team-4.jpg" class="img-fluid" alt=""/>
-                                <div class="member-info">
-                                    <div class="member-info-content">
-                                    <h4>Amanda Jepson</h4>
-                                    <span>Accountant</span>
-                                    <div class="social">
-                                        <a href="/"><i class="fa fa-twitter"></i></a>
-                                        <a href="/"><i class="fa fa-facebook"></i></a>
-                                        <a href="/"><i class="fa fa-google-plus"></i></a>
-                                        <a href="/"><i class="fa fa-linkedin"></i></a>
-                                    </div>
+                                    <img src="assets/img/team-4.jpg" class="img-fluid" alt="" />
+                                    <div class="member-info">
+                                        <div class="member-info-content">
+                                            <h4>Amanda Jepson</h4>
+                                            <span>Accountant</span>
+                                            <div class="social">
+                                                <a href="/"><i class="fa fa-twitter"></i></a>
+                                                <a href="/"><i class="fa fa-facebook"></i></a>
+                                                <a href="/"><i class="fa fa-google-plus"></i></a>
+                                                <a href="/"><i class="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
 
+                            </div>
+                            <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                                <div class="member">
+                                    <img src="assets/img/team-4.jpg" class="img-fluid" alt="" />
+                                    <div class="member-info">
+                                        <div class="member-info-content">
+                                            <h4>Amanda Jepson</h4>
+                                            <span>Accountant</span>
+                                            <div class="social">
+                                                <a href="/"><i class="fa fa-twitter"></i></a>
+                                                <a href="/"><i class="fa fa-facebook"></i></a>
+                                                <a href="/"><i class="fa fa-google-plus"></i></a>
+                                                <a href="/"><i class="fa fa-linkedin"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
 
                     </div>
