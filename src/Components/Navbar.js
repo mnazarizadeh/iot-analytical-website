866ee21ed9e66d6742994b3c92642705bed55f5b
Ali Sadoughi <asadoughi1990@gmail.com>
2020-03-01 20:20:24 +0330

    login button fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -111,40 +111,63 @@ class Navbar extends Component {
             <a className="navbar-brand" href="/">
               <h3 id="logo-text">IOT ANALYTICAL</h3>
             </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#intro">Home<span className="sr-only">(current)</span></a>
+                  <a className="nav-link" href="#intro">
+                    Home<span className="sr-only">(current)</span>
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#about">Features</a>
+                  <a className="nav-link" href="#about">
+                    Features
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#portfolio">Portfolio</a>
+                  <a className="nav-link" href="#portfolio">
+                    Portfolio
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#team">Team</a>
+                  <a className="nav-link" href="#team">
+                    Team
+                  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link mr-3" href="#contact">Contact Us</a>
+                  <a className="nav-link mr-3" href="#contact">
+                    Contact Us
+                  </a>
                 </li>
               </ul>
 
-              <button className="btn btn-outline-primary bt-md my-2 my-sm-0 px-4"
-                onClick={() => this.openModal()}>Log in</button>
-
+              <button
+                id="btn-login"
+                className="btn my-2 my-sm-0 px-4"
+                onClick={() => this.openModal()}
+              >
+                Log in
+              </button>
             </div>
           </div>
         </nav>
-
       </React.Fragment>
     );
   }
 }
 
 export default withRouter(Navbar);
-
