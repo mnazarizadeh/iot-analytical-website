bff4dda2d3152216e6b7f2dc8a21fed25ab91a02
Ali Sadoughi <asadoughi1990@gmail.com>
2020-03-01 20:41:14 +0330

    scroll up button fixed

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -2,14 +2,14 @@ import React from "react";
 import ScrollUpButton from "react-scroll-up-button";
 
 export default class ScrollToTop extends React.Component {
-    render() {
-        return (
-            <div>
-                <ScrollUpButton />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ScrollUpButton
+          ContainerClassName="MyOverRideClass"
+          style={{ background: "#007bff" }}
+        />
+      </div>
+    );
+  }
 }
-
-
-
