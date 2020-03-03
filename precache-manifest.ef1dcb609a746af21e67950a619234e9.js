0de59c50995b4a628a73e6774881dd9a72b86efb
DESKTOP-U5GUPOI\mahdi <m.niknya@gmail.com>
2020-03-03 17:17:35 +0330

    Updates

---
+++ precache-manifest.ef1dcb609a746af21e67950a619234e9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff45e785247cc9648d090d9792ae624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56e011b0b639135161f",
+    "url": "/static/css/2.05f6aeff.chunk.css"
+  },
+  {
+    "revision": "10aeff2a2400a571d646",
+    "url": "/static/css/main.90d7f943.chunk.css"
+  },
+  {
+    "revision": "c56e011b0b639135161f",
+    "url": "/static/js/2.a65a9d43.chunk.js"
+  },
+  {
+    "revision": "3313ff2ed6bc4c601ef109bd9415fd8d",
+    "url": "/static/js/2.a65a9d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10aeff2a2400a571d646",
+    "url": "/static/js/main.38f2f872.chunk.js"
+  },
+  {
+    "revision": "67d2f5ba5143fe1affa48e55357bf584",
+    "url": "/static/js/main.38f2f872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95203427d3ba6989e838",
+    "url": "/static/js/runtime-main.f69f45de.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "20e8f8600db51020010f62b7cc172419",
+    "url": "/static/media/intro-bg.20e8f860.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iot-analytical-website/precache-manifest.44550c2031cc1198a5ae93a6a4def5df.js"
+  "/precache-manifest.ef1dcb609a746af21e67950a619234e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iot-analytical-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
