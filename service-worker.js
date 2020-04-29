d5d688959a587697c3dfd9d3c0791c8ea09732f8
Ali Sadoughi <asadoughi1990@gmail.com>
2020-04-30 00:48:45 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b6bdcf0c4f520c64cda94408f163027.js"
+  "/precache-manifest.4eb6f745c4f08bfbba2a4d79cf6bf682.js"
 );
 
 self.addEventListener('message', (event) => {
