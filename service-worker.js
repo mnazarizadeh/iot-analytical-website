32c7b3a9531e54d6bbdd726fe2107e8e39d19834
Mohammad <m.nazarizadeh@gmail.com>
2020-05-05 22:11:22 +0430

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c45b530e49247b786ccc2274184ebf49.js"
+  "/precache-manifest.47aa6ce6e577a301661d9249c730ff63.js"
 );
 
 self.addEventListener('message', (event) => {
